97aa7458ace7f142475804319e22c2b6f49e23e6
fachdf <fachridhia.f@gmail.com>
2021-03-22 11:28:32 +0700

    Throwing Exceptions - Finish

---
+++ Week 2/Throwing Exceptions/src/main/MathUtils.java
@@ -0,0 +1,27 @@
+package main;
+
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils
+{
+    //-------------------------------------------------------------
+    // Returns the factorial of the argument given
+    //-------------------------------------------------------------
+    public static int factorial(int n) throws IllegalArgumentException
+    {
+        if (n < 0){
+            throw new IllegalArgumentException("Angka Minus");
+        }else if (n > 16){
+            throw new IllegalArgumentException("Angka lebih dari 16");
+        }
+        int fac = 1;
+        for (int i=n; i>0; i--){
+        fac *= i;
+        }
+        return fac;
+    }
+}
